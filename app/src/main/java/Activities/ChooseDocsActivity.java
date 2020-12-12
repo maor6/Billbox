34b228d190e24efcf209ca39f1f90da70f8487ff
gofna <gofnaivry90@gmail.com>
2020-12-13 01:18:33 +0200

    Change files name

---
+++ app/src/main/java/Activities/ChooseDocsActivity.java
@@ -0,0 +1,40 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ *  This is an activity class to choose the type of document to create
+ */
+public class ChooseDocsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CardView receiptBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_docs);
+
+        initActivity();
+    }
+
+    /**
+     * this function initialize the views in the screen
+     */
+    private void initActivity() {
+        receiptBtn = (CardView) findViewById(R.id.receiptChoose);
+        receiptBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == receiptBtn.getId()) {
+            startActivity(new Intent(this, CreateBillActivity.class));
+        }
+    }
+}

79854c1376b081bea91dc39ef9da7d9f10bc38f8
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2020-12-21 00:15:20 +0200

    Fix api

--- app/src/main/java/com/example/myapplication/SendNotificationPack/APIService.java
+++ app/src/main/java/com/example/myapplication/SendNotificationPack/APIService.java
@@ -9,7 +9,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAL5VJ9i8:APA91bFpD_7O9GGkKHUO9qGrC4ZbZdp5n_vrScQq5ZYTL6IXj_HzT9Lsp5aRtw1auBIJwlk0dn-G-wBWQmhi-zQXkJw4vR0Z0FpUlVwOptLX3K_ZtlB3cU1IDC_hYzOOFc9q-lyA92cX"
+                    "Authorization:key=xxxxxxxxxxxxxxxxxxxxxxxx"
             }
     )
 

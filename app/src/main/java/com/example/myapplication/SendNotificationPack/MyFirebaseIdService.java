d3b81b5257063b6f58404dfe3b2b20df9ade88fb
DESKTOP-K3BSLJJ\Maor <maor.ovadia6@gmail.com>
2020-12-22 13:17:02 +0200

    Update MyFirebaseService

--- app/src/main/java/com/example/myapplication/SendNotificationPack/MyFirebaseIdService.java
+++ app/src/main/java/com/example/myapplication/SendNotificationPack/MyFirebaseIdService.java
@@ -3,25 +3,23 @@ package com.example.myapplication.SendNotificationPack;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
-import java.util.Objects;
 
 public class MyFirebaseIdService extends FirebaseMessagingService {
 
     @Override
-    public void onNewToken(String s)
+    public void onNewToken(String token)
     {
-        super.onNewToken(s);
+        super.onNewToken(token);
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        String refreshToken = FirebaseInstanceId.getInstance().getToken();
         if(firebaseUser != null) {
-            updateToken(refreshToken);
+            updateToken(token);
         }
     }
+
     private void updateToken(String refreshToken){
         Token token1 = new Token(refreshToken);
-        FirebaseDatabase.getInstance().getReference("Tokens").child(Objects.requireNonNull(FirebaseAuth.getInstance()
-                .getCurrentUser()).getUid()).setValue(token1);
+        FirebaseDatabase.getInstance().getReference("Tokens").child(FirebaseAuth.getInstance()
+                .getCurrentUser().getUid()).setValue(token1);
     }
 }

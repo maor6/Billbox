a363f0818a9bb73ba62f0a10b3cbe73484df4fdc
gofna <gofnaivry90@gmail.com>
2020-12-23 16:45:49 +0200

    Add new files to customer

---
+++ app/src/main/java/Activities/WarrantiesCustomerActivity.java
@@ -0,0 +1,13 @@
+package Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WarrantiesCustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warranties_customer);
+    }
+}
